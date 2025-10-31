9c5c2b988a102d31b7bfdc23fbf5de0b5db383b7
amit04-07-1999 <ameykumar76@gmail.com>
2025-10-31 16:32:02 +0530

    gg

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/gads/nov25/delhi/',
   plugins: [
     react(),
     tailwindcss(),
